901422e615834ce7358d67911d6553ff4f6d5f47
CharleneMSI <q2330416@gmail.com>
2024-06-20 08:23:03 -0500

    Ori

---
+++ Demo_Seurat_scATAC.R
@@ -0,0 +1,12 @@
+library(SeuratData)
+InstallData("pbmcMultiome")
+library(Seurat)
+library(Signac)
+library(EnsDb.Hsapiens.v86)
+library(ggplot2)
+library(cowplot)
+
+library(SeuratData)
+library(Seurat)
+pbmc.rna <- LoadData("pbmcMultiome", "pbmc.rna")
+pbmc.atac <- LoadData("pbmcMultiome", "pbmc.atac")
