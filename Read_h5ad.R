1c287fa088982171e1f21381f7ac596c64abab38
CharleneMSI <q2330416@gmail.com>
2024-06-19 23:20:31 -0500

    h5ad file

---
+++ Read_h5ad.R
@@ -0,0 +1,8 @@
+library(zellkonverter)
+
+library(reticulate)
+use_condaenv("zellkonverterAnnDataEnv")  # 使用適當的 Conda 環境
+anndata <- import("anndata")
+
+# 嘗試讀取 .h5ad 檔案
+adata <- readH5AD("C:/Users/user/Downloads/2aa90e63-9a6d-444d-8343-8fc2a9921797.h5ad")
