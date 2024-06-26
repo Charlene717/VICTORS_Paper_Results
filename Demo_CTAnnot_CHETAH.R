75202814d2a450cd3afe6d34014f3f79bddf3af0
CharleneMSI <q2330416@gmail.com>
2024-06-26 08:54:14 -0500

    Checkpoint

--- Demo_CTAnnot_CHETAH.R
+++ Demo_CTAnnot_CHETAH.R
@@ -15,21 +15,67 @@ if(!require("tidyverse")) install.packages("tidyverse"); library(tidyverse)
 ## BiocManager
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 if(!require("SingleCellExperiment")) BiocManager::install("SingleCellExperiment"); library(SingleCellExperiment)
+if(!require("CHETAH")) BiocManager::install("CHETAH"); library(CHETAH)
 
 
-## Make SingleCellExperiments
-reference <- SingleCellExperiment(assays = list(counts = ref_counts),
-                                  colData = DataFrame(celltypes = ref_ct))
+#### Load Data ####
+load("D:/Dropbox/##_GitHub/###_VUMC/CreateDataset/Input_Dataset/Seurat_pbmcMultiome/Seurat_pbmcMultiome_Preprocessing.RData")
 
-input <- SingleCellExperiment(assays = list(counts = input_counts),
-                              reducedDims = SimpleList(TSNE = input_tsne))
+seuratObject_Sample <- pbmc.rna
+seuratObject_Ref <- pbmc.rna
 
-## Run CHETAH
-input <- CHETAHclassifier(input = input, ref_cells = reference)
 
-## Plot the classification
-PlotCHETAH(input)
+# Convert Seurat objects to SingleCellExperiment
+ref_sce <- as.SingleCellExperiment(seuratObject_Ref)
+sample_sce <- as.SingleCellExperiment(seuratObject_Sample)
 
-## Extract celltypes:
-celltypes <- input$celltype_CHETAH
 
+# Prepare Reference Data
+ref_sce$celltypes <- seuratObject_Ref@meta.data[["seurat_annotations"]]
+
+
+# Run CHETAH classifier
+sample_sce <- CHETAHclassifier(input = sample_sce, ref_cells = ref_sce)
+
+
+# Plot classification
+PlotCHETAH(sample_sce)
+
+# Extract cell types
+celltypes <- sample_sce$celltype_CHETAH
+
+
+# Update Seurat Object
+seuratObject_Sample$predicted_celltype <- celltypes
+
+plot_CHETAH <- DimPlot(seuratObject_Sample,group.by = "predicted_celltype", reduction = "umap")
+plot_seurat <- DimPlot(seuratObject_Sample,group.by = "seurat_annotations", reduction = "umap")
+plot_CHETAH + plot_seurat
+
+
+
+
+
+
+##################################################################################
+# # https://www.bioconductor.org/packages/devel/bioc/vignettes/CHETAH/inst/doc/CHETAH_introduction.html
+#
+# #### Load Data ####
+# load("D:/Dropbox/##_GitHub/###_VUMC/CreateDataset/Input_Dataset/CHETAH_TME/CHETAH_TME_reference.Rdata")
+#
+# ## Make SingleCellExperiments
+# reference <- SingleCellExperiment(assays = list(counts = ref_counts),
+#                                   colData = DataFrame(celltypes = ref_ct))
+#
+# input <- SingleCellExperiment(assays = list(counts = input_counts),
+#                               reducedDims = SimpleList(TSNE = input_tsne))
+#
+# ## Run CHETAH
+# input <- CHETAHclassifier(input = input, ref_cells = reference)
+#
+# ## Plot the classification
+# PlotCHETAH(input)
+#
+# ## Extract celltypes:
+# celltypes <- input$celltype_CHETAH
+#
