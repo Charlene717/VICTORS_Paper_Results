101acafed6a935337f4dedf280e752ffcefd266b
CharleneMSI <q2330416@gmail.com>
2024-06-26 11:26:38 -0500

    Debug: install Azimuth

--- Demo_CTAnnot_Azimuth.R
+++ Demo_CTAnnot_Azimuth.R
@@ -13,11 +13,47 @@ memory.limit(150000)
 if(!require("Seurat")) install.packages("Seurat"); library(Seurat)
 if(!require("tidyverse")) install.packages("tidyverse"); library(tidyverse)
 
+# pkgbuild::check_build_tools(debug = TRUE)
 # Download and Install Rtools: https://cran.r-project.org/bin/windows/Rtools/
 # if (!requireNamespace("pkgbuild", quietly = TRUE)) install.packages("pkgbuild")
-# pkgbuild::check_build_tools(debug = TRUE)
+## BiocManager
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+# BiocManager::install(c("BSgenome.Hsapiens.UCSC.hg38", "TFBSTools"))
 if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
 if(!require("Azimuth")) devtools::install_github("satijalab/azimuth", "seurat5"); library(Azimuth)
+
+## GitHub
+if(!require("remotes")) install.packages("remotes"); library(remotes)
+if(!require("SeuratData")) remotes::install_github("satijalab/seurat-data"); library(SeuratData)
+
+
+# Install the PBMC systematic comparative analyis (pmbcsca) dataset
+InstallData("pbmcsca")
+
+# returns a Seurat object named pbmcsca
+pbmcsca <- LoadData("pbmcsca")
+
+# The RunAzimuth function can take a Seurat object as input
+pbmcsca <- RunAzimuth(pbmcsca, reference = "pbmcref")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #### Load Data ####
 load("D:/Dropbox/##_GitHub/###_VUMC/CreateDataset/Input_Dataset/Seurat_pbmcMultiome/Seurat_pbmcMultiome_Preprocessing.RData")
