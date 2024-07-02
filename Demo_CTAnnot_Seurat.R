b88148003c834fbb8bb1426360c047b3bdc49ad1
CharleneMSI <q2330416@gmail.com>
2024-07-02 04:56:16 -0500

    Load Data

--- Demo_CTAnnot_Seurat.R
+++ Demo_CTAnnot_Seurat.R
@@ -17,9 +17,14 @@ if(!require("Seurat")) install.packages("Seurat"); library(Seurat)
 if(!require("tidyverse")) install.packages("tidyverse"); library(tidyverse)
 if(!require("caret")) install.packages("caret"); library(caret)
 
-# 加載必要的包
-library(Seurat)
-library(caret)
+#### Load Data ####
+load("D:/Dropbox/##_GitHub/###_VUMC/CreateDataset/Input_Dataset/Seurat_pbmcMultiome/Seurat_pbmcMultiome_Preprocessing.RData")
+
+seuratObject_Sample <- pbmc.rna
+seuratObject_Ref <- pbmc.rna
+seuratObject_Ref@meta.data[["Actual_Cell_Type"]] <- seuratObject_Ref@meta.data[["seurat_annotations"]]
+
+
 
 # 1. 準備和預處理數據
 # 預處理參考數據
