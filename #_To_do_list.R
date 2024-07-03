d402af4045b0c5fb4678ec6917bdec88c7b3cac0
CharleneMSI <q2330416@gmail.com>
2024-07-03 03:56:18 -0500

    VICTOR

--- #_To_do_list.R
+++ #_To_do_list.R
@@ -1,14 +1,24 @@
 ## To-do list
 
+
+
+
+
+
+
+
+################################################################################
+
+
 # -[v] 設定是否要新增預設閾值
-# -[v] 評估VICTORS結果程式碼整理
-# -[v] VICTORS結果視覺化輸出
+# -[v] 評估VICTOR結果程式碼整理
+# -[v] VICTOR結果視覺化輸出
 
-# -[] VICTORS結果視覺化程式碼整理
+# -[] VICTOR結果視覺化程式碼整理
 
-# -[] VICTORS Readme Demo
+# -[v] VICTOR Readme Demo
 
-# -[] VICTORS Paper總程式整理
+# -[] VICTOR Paper總程式整理
 
-# -[] VICTORS Package
+# -[v] VICTOR Package
 
