0fa610a0fb32bf2004e3d39541722dadadf964ab
CharleneMSI <q2330416@gmail.com>
2024-07-03 03:42:01 -0500

    Add Azimuth

--- Demo_CTAnnot_Seurat.R
+++ Demo_CTAnnot_Seurat.R
@@ -1,3 +1,48 @@
+# Seurat V5 Azimuth annotation
+# https://satijalab.github.io/azimuth/articles/run_azimuth_tutorial.html
+# https://azimuth.hubmapconsortium.org/
+#   Azimuth outputs Prediction scores, which range from 0 to 1 and reflect the confidence associated with each annotation. However, Azimuth does not provide a recommended threshold. Can I set it at 0.8 for our tests?
+
+##### Presetting ######
+rm(list = ls()) # Clean variable ##* Comment out if Run All
+memory.limit(150000)
+
+
+#### Load Packages ####
+## Load packages by CRAN
+if(!require("Seurat")) install.packages("Seurat"); library(Seurat)
+if(!require("tidyverse")) install.packages("tidyverse"); library(tidyverse)
+
+# pkgbuild::check_build_tools(debug = TRUE)
+# Download and Install Rtools: https://cran.r-project.org/bin/windows/Rtools/
+# if (!requireNamespace("pkgbuild", quietly = TRUE)) install.packages("pkgbuild")
+## BiocManager
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+# BiocManager::install(c("BSgenome.Hsapiens.UCSC.hg38", "TFBSTools"))
+if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+if(!require("Azimuth")) devtools::install_github("satijalab/azimuth", "seurat5"); library(Azimuth)
+
+## GitHub
+if(!require("remotes")) install.packages("remotes"); library(remotes)
+if(!require("SeuratData")) remotes::install_github("satijalab/seurat-data"); library(SeuratData)
+
+
+# Install the PBMC systematic comparative analyis (pmbcsca) dataset
+InstallData("pbmcsca")
+
+# returns a Seurat object named pbmcsca
+pbmcsca <- LoadData("pbmcsca")
+
+# The RunAzimuth function can take a Seurat object as input
+pbmcsca <- RunAzimuth(pbmcsca, reference = "pbmcref")
+
+
+p1 <- DimPlot(pbmcsca, group.by = "predicted.celltype.l2", label = TRUE, label.size = 3) + NoLegend()
+p2 <- DimPlot(pbmcsca, group.by = "Method")
+p1 + p2
+
+################################################################################
+
 # Mapping and annotating query datasets
 # https://satijalab.org/seurat/articles/integration_mapping
 
