76987835b0e6786f38e8e3a70a639413a808698a
CharleneMSI <q2330416@gmail.com>
2024-07-07 03:15:11 -0500

    Check point

--- Demo_Cell_Type_Annotation.R
+++ Demo_Cell_Type_Annotation.R
@@ -37,6 +37,10 @@ ref_cells <- sample(cells, length(cells) / 5)
 seuratObject_Sample <- subset(pbmc.rna, cells = sample_cells)
 seuratObject_Ref <- subset(pbmc.rna, cells = ref_cells)
 
+# Set Actual_Cell_Type in seuratObject_Sample
+seuratObject_Sample@meta.data[["Actual_Cell_Type"]] <- seuratObject_Sample@meta.data[["seurat_annotations"]]
+
+
 # Set Actual_Cell_Type in seuratObject_Ref
 seuratObject_Ref@meta.data[["Actual_Cell_Type"]] <- seuratObject_Ref@meta.data[["seurat_annotations"]]
 
@@ -69,7 +73,44 @@ seuratObject_Sample <- Run_Seurat_Annot(seuratObject_Sample, seuratObject_Ref)
 # cellTypes.reclassify <- Fun_scReClassify(seuratObject_Sample, seuratObject_Ref)
 
 
-#### Run Cell Type Annotation ####
+#### DiagnosticMetrics ####
+source("FUN_Metrics_CellTypeAnnot.R")
+
+## singleR
+seuratObject_Sample <- FUN_DiagnosticMetrics(seuratObject_Sample, "Actual_Cell_Type",
+                                             "label_singleR_NoReject", "label_singleR")
+
+## scmap
+seuratObject_Sample <- FUN_DiagnosticMetrics(seuratObject_Sample, "Actual_Cell_Type",
+                                             "label_scmap_NoReject", "label_scmap")
+
+## SCINA
+seuratObject_Sample <- FUN_DiagnosticMetrics(seuratObject_Sample, "Actual_Cell_Type",
+                                             "label_SCINA_NoReject", "label_SCINA")
+## scPred
+seuratObject_Sample <- FUN_DiagnosticMetrics(seuratObject_Sample, "Actual_Cell_Type",
+                                             "label_scPred_NoReject", "label_scPred")
+
+## CHETAH
+seuratObject_Sample <- FUN_DiagnosticMetrics(seuratObject_Sample, "Actual_Cell_Type",
+                                             "label_CHETAH_NoReject", "label_CHETAH")
+
+## scClassify
+seuratObject_Sample <- FUN_DiagnosticMetrics(seuratObject_Sample, "Actual_Cell_Type",
+                                             "label_scClassify_NoReject", "label_scClassify")
+
+## Seurat
+seuratObject_Sample <- FUN_DiagnosticMetrics(seuratObject_Sample, "Actual_Cell_Type",
+                                             "label_Seurat_NoReject", "label_Seurat")
 
 
+#### Visualization ####
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_singleR_NoReject")
 
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_singleR_DiagPara")
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_scmap_DiagPara")
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_SCINA_DiagPara")
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_scPred_DiagPara")
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_CHETAH_DiagPara")
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_scClassify_DiagPara")
+DimPlot(seuratObject_Sample, reduction = "umap", group.by = "label_Seurat_DiagPara")
