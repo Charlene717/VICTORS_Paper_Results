0cdd82453db81257ceb8b4f84ba67884e1499707
CharleneMSI <q2330416@gmail.com>
2024-07-09 04:42:13 -0500

    Remove To-do list

--- Demo_Cell_Type_Annotation.R
+++ Demo_Cell_Type_Annotation.R
@@ -397,29 +397,3 @@ dev.off()
 
 ################################################################################
 
-#### To-do list ####
-## -[V] Metric_Other
-
-## -[V] Function name
-
-## -[L] VICTOR
-## -[L] VICTOR_glmnet
-## -[V] VICTOR_StatROC
-## -[V] VICTOR_misc
-## -[L] VICTOR_Prediction
-## -[V] VICTOR_SeuratV5_SeuratV4
-
-## -[] Fig Diag Accuracy
-
-## -[] ATAC-seq
-## -[] Sampling
-## -[] Old sample
-## -[] Big Data
-
-## -[] UMAP
-
-## -[] scPred_SeuratV5
-
-## -[] Reference Setting
-## -[] Cell Type Name Function
-## -[] Cell Type Name WO _
