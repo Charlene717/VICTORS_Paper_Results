be557a5ba627c65a892ca1d12298745a9cd14056
CharleneMSI <q2330416@gmail.com>
2024-07-09 04:42:37 -0500

    To-do list

---
+++ #_To_do_list_20240709.R
@@ -0,0 +1,26 @@
+#### To-do list ####
+## -[V] Metric_Other
+
+## -[V] Function name
+
+## -[L] VICTOR
+## -[L] VICTOR_glmnet
+## -[V] VICTOR_StatROC
+## -[V] VICTOR_misc
+## -[L] VICTOR_Prediction
+## -[V] VICTOR_SeuratV5_SeuratV4
+
+## -[] Fig Diag Accuracy ConfStat
+
+## -[] ATAC-seq
+## -[] Sampling
+## -[] Old sample
+## -[] Big Data
+
+## -[] UMAP
+
+## -[] scPred_SeuratV5
+
+## -[] Reference Setting
+## -[] Cell Type Name Function
+## -[] Cell Type Name WO _
