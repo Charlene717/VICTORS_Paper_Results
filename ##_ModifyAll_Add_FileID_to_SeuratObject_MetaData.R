ff827f254a223a4b11fd1eb573ac99087cb00c3d
CharleneMSI <q2330416@gmail.com>
2024-07-12 03:23:24 -0500

    Export MetaData

--- ##_ModifyAll_Add_FileID_to_SeuratObject_MetaData.R
+++ ##_ModifyAll_Add_FileID_to_SeuratObject_MetaData.R
@@ -13,8 +13,8 @@ if(!require("readr")) install.packages("readr"); library(readr)
 
 #### Set Loading ###
 # 設定主目錄
-# main_directory <- "D:/Dropbox/##_GitHub/###_VUMC/VICTORS_Paper_Results/Export_GSE132044_20240710"
-main_directory <- "D:/Dropbox/##_GitHub/###_VUMC/VICTORS_Paper_Results/Export_scRNAseqPanc_20240711"
+main_directory <- "D:/Dropbox/##_GitHub/###_VUMC/VICTORS_Paper_Results/Export_GSE132044_20240710_S"
+# main_directory <- "D:/Dropbox/##_GitHub/###_VUMC/VICTORS_Paper_Results/Export_scRNAseqPanc_20240711"
 
 # 獲取所有子目錄
 subdirectories <- list.dirs(main_directory, recursive = TRUE, full.names = TRUE)
@@ -30,8 +30,17 @@ for (subdir in subdirectories) {
   # 讀取並合併這些檔案
   for (file in RData_files) {
     load(file)
-    seuratObject_Sample@meta.data$FileID <- Name_FileID
-    save.image(file)
+
+    # ## Add Name_FileID to meta.data
+    # seuratObject_Sample@meta.data$FileID <- Name_FileID
+    # save.image(file)
+
+    ## Export MetaData
+    write.table(data.frame(ID=rownames(seuratObject_Sample@meta.data), seuratObject_Sample@meta.data),
+                file=paste0(main_directory,"/",Name_ExportFolder,"/",Name_Export,"_metadataSamp.tsv"),
+                # file=paste0(Name_time_wo_micro,"_metadataSamp.tsv"),
+                quote = FALSE,row.names = FALSE,col.names = TRUE, na = "",sep = '\t')
+
   }
 }
 
