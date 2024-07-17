50db189f3be77accac7c08e3e02558f854a859cd
CharleneMSI <q2330416@gmail.com>
2024-07-17 07:59:30 -0500

    Load Packages

--- #_Run_scReClassify.R
+++ #_Run_scReClassify.R
@@ -1,2 +1,18 @@
 # PMID31874628 scReClassify
 # https://bioconductor.org/packages/release/bioc/vignettes/scReClassify/inst/doc/scReClassify.html
+
+
+# ##### Presetting ######
+# rm(list = ls()) # Clean variable ##* Comment out if Run All
+# memory.limit(150000)
+
+#### Load Packages ####
+# Load necessary packages
+if(!require("Seurat")) install.packages("Seurat"); library(Seurat)
+if(!require("tidyverse")) install.packages("tidyverse"); library(tidyverse)
+if(!require("caret")) install.packages("caret"); library(caret)
+
+if (!require("scReClassify")) BiocManager::install("scReClassify"); library(scReClassify)
+if (!require("DT")) install.packages("DT"); library(DT)
+if (!require("mclust")) install.packages("mclust"); library(mclust)
+if(!require("SingleCellExperiment"))  BiocManager::install("SingleCellExperiment"); library(SingleCellExperiment)
