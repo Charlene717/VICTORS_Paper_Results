54406085df42fc429a1e41a737e5f2556ac0a1bd
CharleneMSI <q2330416@gmail.com>
2024-07-17 06:27:02 -0500

    # Checkpoint: Fun_scReClassify

--- #_FUN_CellTypeAnnot.R
+++ #_FUN_CellTypeAnnot.R
@@ -410,84 +410,44 @@ Run_Seurat_Annot <- function(Query_Seurat, Reference_Seurat,
 
 ################################################################################
 #### scReClassify ####
-Fun_scReClassify <- function(Query_Seurat, Reference_Seurat,
-                             Set_classifier = "svm", Set_percent = 1,
-                             Set_L = 10, ...) {
-  # PMID31874628 scReClassify # https://bioconductor.org/packages/release/bioc/vignettes/scReClassify/inst/doc/scReClassify.html
+Fun_scReClassify <- function(Query_Seurat, Set_RefAnnoCol = "Annotation",
+                             Set_classifier = "svm", Set_percent = 1, Set_L = 10) {
 
-  # Load necessary packages
-  if (!require("scReClassify")) BiocManager::install("scReClassify"); library(scReClassify)
-  if (!require("DT")) install.packages("DT"); library(DT)
-  if (!require("mclust")) install.packages("mclust"); library(mclust)
-
+  #### scReClassify ####
   # Convert Seurat objects to SingleCellExperiment
-  # ref_sce <- as.SingleCellExperiment(Reference_Seurat)
   sample_sce <- as.SingleCellExperiment(Query_Seurat)
 
   # Standardize the data and create 'logNorm' assay
-  # Log-normalize dataset if not already done
-  if (is.null(Reference_Seurat@assays[["RNA"]]@layers[["data"]])) { ref_sce <- logNormCounts(ref_sce)}
-  if (is.null(Query_Seurat@assays[["RNA"]]@layers[["data"]])) { sample_sce <- logNormCounts(sample_sce)}
+  sample_sce <- scater::logNormCounts(sample_sce)
 
-
-  # # Create 'logNorm' layer in assays
-  # SummarizedExperiment::assay(ref_sce, "logNorm") <- SummarizedExperiment::assay(ref_sce, "logcounts")
+  # Create 'logNorm' layer in assays
   SummarizedExperiment::assay(sample_sce, "logNorm") <- SummarizedExperiment::assay(sample_sce, "logcounts")
 
   # Remove constant rows
   remove_constant_rows <- function(mat) { mat[rowSums(mat != 0) > 0, ]}
 
-  # logNorm_ref <- remove_constant_rows(assay(ref_sce, "logNorm"))
-  logNorm_sample <- remove_constant_rows(assay(sample_sce, "logNorm"))
+  logNorm_sample <- remove_constant_rows(SummarizedExperiment::assay(sample_sce, "logNorm"))
 
-
-  # ## Dimension reduction
-  # pca_ref <- stats::prcomp(t(logNorm_ref), center = TRUE, scale. = TRUE) # reducedDim(ref_sce, "matPCs") <- matPCs(ref_sce, assay = "logNorm", 0.7)
-  pca_sample <- stats::prcomp(t(logNorm_sample), center = TRUE, scale. = TRUE) # reducedDim(sample_sce, "matPCs") <- matPCs(sample_sce, assay = "logNorm", 0.7)
-
-  # reducedDim(ref_sce, "matPCs") <- pca_ref$x
+  ## Dimension reduction
+  pca_sample <- stats::prcomp(t(logNorm_sample), center = TRUE, scale. = TRUE)
   reducedDim(sample_sce, "matPCs") <- pca_sample$x
 
-  # Cell types
+  ## Set Cell types
+  sample_sce[["cellTypes"]] <- sample_sce[[Set_RefAnnoCol]]
+  names(sample_sce@colData@listData[["cellTypes"]]) <- row.names(Query_Seurat@meta.data)
   cellTypes <- sample_sce[["cellTypes"]]
-
-
-  # Check for NA values and remove them from ref_sce
-  valid_cells <- !is.na(cellTypes) & complete.cases(reducedDim(sample_sce, "matPCs"))
-  cellTypes <- cellTypes[valid_cells]
-  sample_sce <- sample_sce[, valid_cells]
-
-  # Check for balanced classes and adjust if necessary in sample_sce
-  cellType_counts <- table(cellTypes)
-  LimNum <- 5
-  if (any(cellType_counts < LimNum)) {
-    warning(paste0("Some classes in reference have fewer than ",LimNum," samples. This may cause issues with the classifier."))
-    min_class <- names(cellType_counts[cellType_counts < LimNum])
-    for (cls in min_class) {
-      sample_sce <- sample_sce[, cellTypes != cls]
-      cellTypes <- cellTypes[cellTypes != cls]
-    }
-  }
-
 
   # Run scReClassify
   set.seed(123)
   cellTypes.reclassify <- multiAdaSampling(sample_sce, cellTypes, reducedDimName = "matPCs",
                                            classifier = Set_classifier, percent = Set_percent, L = Set_L)
 
-
   # Save the new annotations to meta.data
-  Query_Seurat$label_scReClassify <- cellTypes.reclassify$final
-
+  Query_Seurat[[paste0("ReAnnot_scReClassify_", Set_RefAnnoCol)]] <- cellTypes.reclassify$final
 
   return(Query_Seurat)
 }
 
-# # Example usage
-# Query_Seurat <- seuratObject_Sample
-# Query_Seurat@meta.data$cellTypes <- Query_Seurat@meta.data$seurat_annotations
-# # Query_Seurat@meta.data$cellTypes <- gsub("_", "  ", Query_Seurat@meta.data$cellTypes)
-#
-# Reference_Seurat <- seuratObject_Ref
-# seuratObject_Sample_T <- Fun_scReClassify(seuratObject_Sample, seuratObject_Ref)
-#
+# ## Example usage:
+# # Query_Seurat <- run_scReClassify(Query_Seurat, Set_RefAnnoCol = "label_singleR", Set_classifier = "svm", Set_percent = 1, Set_L = 10)
+# Query_Seurat <- run_scReClassify(Query_Seurat, Set_RefAnnoCol = "Actual_Cell_Type", Set_classifier = "svm", Set_percent = 1, Set_L = 10)
