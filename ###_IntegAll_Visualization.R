2b2e7d2be9acc0f3f8024957a57ad804f6479b31
CharleneMSI <q2330416@gmail.com>
2024-08-17 04:28:42 -0500

    Modify custom_order of Fig legend

--- ###_IntegAll_Visualization.R
+++ ###_IntegAll_Visualization.R
@@ -168,8 +168,11 @@ create_combind_plot <- function(plots, ncol, title_data_10x, legend_data, color_
 
   # Create a dummy data frame for the legend with a single point
   legend_data <- data.frame(Method = names(color_legend), x = 1, y = 1)
-  custom_order <- c("singleR",  "scmap", "SCINA", "scPred",
-                    "CHETAH","scClassify","Seurat","VICTOR" )
+  # custom_order <- c("singleR",  "scmap", "SCINA", "scPred",
+  #                   "CHETAH","scClassify","Seurat","VICTOR" )
+  custom_order <- c("singleR","scPred","Seurat",
+                    "scmap","CHETAH","VICTOR",
+                    "SCINA", "scClassify")
   legend_data$Method <- factor(legend_data$Method, levels = custom_order)
 
   # Create a ggplot object for the legend without actual points
@@ -180,7 +183,7 @@ create_combind_plot <- function(plots, ncol, title_data_10x, legend_data, color_
     theme(legend.position = "bottom",
           legend.title = element_text(size = 22),
           legend.text = element_text(size = 22)) +
-    guides(color = guide_legend(title = legend_title, override.aes = list(shape = 15,size =6)))
+    guides(color = guide_legend(title = legend_title, override.aes = list(shape = 15,size =6), ncol = 3))
 
   # 创建标题标签
   title_label <- ggdraw() + draw_plot_label(title_data_10x, size = 15, x = 0.5, hjust = 0.5)
