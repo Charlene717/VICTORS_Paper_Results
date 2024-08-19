4d978cfd33d84f92a389fec73aff02f6d2fa8e43
CharleneMSI <q2330416@gmail.com>
2024-08-19 13:19:08 -0500

    Add final_diff

--- Summarize_median_accuracy_for_manuscript.R
+++ Summarize_median_accuracy_for_manuscript.R
@@ -14,13 +14,39 @@ if(!require('readr')) {install.packages('readr'); library(readr)}
 file_path <- "D:\\Dropbox\\##_GitHub\\###_VUMC\\VICTORS_Paper_Results\\#_Revision_20240818_Fin\\Figure 3 GSE132044 Within-platform\\IntegrateAll_20240817164107OSN_GSE132044_SamePlatform.tsv"
 data <- read_tsv(file_path)
 
-# 計算VICTOR對上對應方法的Median Accuracy差異
+# 計算VICTOR對上對應方法的Median Accuracy差異並轉換為百分比形式
 victor_diff <- data %>%
   filter(grepl("_VICTOR", Method)) %>%  # 選擇VICTOR方法
   mutate(Original_Method = gsub("_VICTOR", "", Method)) %>%  # 去除VICTOR以取得對應的原始方法
   left_join(data, by = c("Mislabel_CellType" = "Mislabel_CellType", "Original_Method" = "Method"), suffix = c("_VICTOR", "_Original")) %>%  # 合併原始方法
-  mutate(Median_Difference = Median_VICTOR - Median_Original) %>%  # 計算Median差異
-  select(Mislabel_CellType, Original_Method, Median_Difference)  # 選取所需的欄位
+  mutate(`Median_Difference(%)` = (Median_VICTOR - Median_Original) * 100) %>%  # 計算Median差異並轉為百分比
+  select(Mislabel_CellType, Original_Method, `Median_Difference(%)`)  # 選取所需的欄位
 
 # 顯示結果
 victor_diff
+
+
+##########
+
+# 排除Mislabel_CellType中的None，並計算每個Original_Method中最大和最小的Median_Difference(%)
+victor_diff_filtered <- victor_diff %>%
+  filter(Mislabel_CellType != "None")  # 排除Mislabel_CellType為None的行
+
+# 找出每個Original_Method中Median_Difference(%)最大的Mislabel_CellType
+max_diff <- victor_diff_filtered %>%
+  group_by(Original_Method) %>%
+  slice(which.max(`Median_Difference(%)`)) %>%
+  mutate(Difference_Type = "Max")
+
+# 找出每個Original_Method中Median_Difference(%)最小的Mislabel_CellType
+min_diff <- victor_diff_filtered %>%
+  group_by(Original_Method) %>%
+  slice(which.min(`Median_Difference(%)`)) %>%
+  mutate(Difference_Type = "Min")
+
+# 合併最大和最小的結果生成新的表格
+final_diff <- bind_rows(max_diff, min_diff) %>%
+  arrange(Original_Method, Difference_Type)
+
+# 顯示結果
+final_diff
