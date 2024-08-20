0e7858f1696af579d20d70678643c63804cad833
CharleneMSI <q2330416@gmail.com>
2024-08-20 05:51:55 -0500

    Clean up the code

--- FigS2_Diagnosis_Bar_GSE132044_PBMC_SamePlatform.R
+++ FigS2_Diagnosis_Bar_GSE132044_PBMC_SamePlatform.R
@@ -13,16 +13,17 @@ if(!require("tidyr")) install.packages("tidyr"); library(tidyr)
 if(!require(cowplot)) install.packages("cowplot"); library(cowplot)
 
 #### Load data ####
-# load("D:/Dropbox/###_VUMC/##_Research/VICTORS/Figures/Fig3_Fig4_GSE132044_PBMC/20240213020617_GSE132044_Sum_SVDLRglmnet_ROC.RData")
-# folder_path <- "D:/Dropbox/###_VUMC/##_Research/VICTORS/Figures/FigS2_GSE132044_PBMC_SamePlatform_Bar/"
-
 load("D:/Dropbox/##_GitHub/###_VUMC/VICTORS_Paper_Results/#_Export_20240717/Export_IntegrateAll_20240715161840IVL_GSE132044/20240715161840IVL_GSE132044_IntegrateAll.RData")
 folder_path <- "D:/Dropbox/##_GitHub/###_VUMC/VICTORS_Paper_Results/Export/"
 
 all_data <- combined_data
 all_data_ori <- all_data
-# all_data <- all_data %>%  filter(Mislabel_CellType != "None")
 
+Summary.df <- data_same_platform
+
+## Remove Nonw
+all_data <- all_data %>%  filter(Mislabel_CellType != "None")
+Summary.df <- Summary.df %>%  filter(Mislabel_CellType != "None")
 
 #### Set Parameter ####
 # Set_Obs_CellType <- "Alpha cell" # "Beta cell" # "Alpha cell" # "Acinar cell"
@@ -31,15 +32,8 @@ Set_Ref_State <- "lack" # "with" #"lack" #"Comp"
 Set_RmUnassign <- FALSE
 
 # 设置参数以指定要比较的方法
-Set_Method <- "scmap" # 示例，可以改为其他方法，如"singleR", "scmap", "SCINA", "scPred"等
+Set_Method <- "singleR" # 示例，可以改为其他方法，如"singleR", "scmap", "SCINA", "scPred"等
 Victors_Method <- paste(Set_Method, "VICTOR", sep = "_")
-
-
-Summary.df <- data_same_platform
-
-# # 刪除 Mislabel_CellType 為 None 的列
-# Summary.df <- Summary.df %>%  filter(Mislabel_CellType != "None")
-
 
 
 #### Data prepocessing ####
@@ -48,9 +42,6 @@ selected_data <- all_data %>%
   select(FileID, Sample_Platform, Ref_Platform, Mislabel_CellType, Actual_Cell_Type, contains("DiagPara"), contains("label")) %>%
   distinct()
 
-# Replace 'unknown' with 'Unassign' in the label_SCINA_NoReject column
-selected_data <- selected_data %>%
-  mutate(label_SCINA_NoReject = ifelse(label_SCINA_NoReject == "unknown", "Unassign", label_SCINA_NoReject))
 
 # 筛选Sample_Platform和Ref_Platform相等的行
 selected_data <- selected_data %>%
@@ -59,7 +50,6 @@ selected_data <- selected_data %>%
 
 library(dplyr)
 library(tidyr)
-
 
 # 第一步：找到差值最大的Mislabel_CellType
 max_diff_cell_type <- Summary.df %>%
@@ -75,33 +65,16 @@ max_diff_cell_type <- Summary.df %>%
 Set_Obs_CellType <- max_diff_cell_type %>% as.character()
 
 # 第二步：根据条件筛选TargetCell_data
-# 注意，这里的selected_data应该是你要从中筛选数据的DataFrame
-# 这一步需要明确selected_data的来源，这里假设它已经定义好了
-if(Set_Ref_State == "with" ){
-  TargetCell_data <- selected_data %>%
-    filter(Mislabel_CellType != Set_Obs_CellType) %>%
-    filter(Actual_Cell_Type == Set_Obs_CellType)
-} else if (Set_Ref_State == "Comp"){
-  TargetCell_data <- selected_data %>%
-    filter(Mislabel_CellType == "None") %>%
-    filter(Actual_Cell_Type == Set_Obs_CellType)
-} else {
-  # TargetCell_data <- selected_data %>%
-  #   filter(Mislabel_CellType == Set_Obs_CellType) %>%
-  #   filter(Actual_Cell_Type == Set_Obs_CellType)
+TargetCell_data <- selected_data %>%
+  filter(Mislabel_CellType == Set_Obs_CellType)
 
-  TargetCell_data <- selected_data %>%
-    filter(Mislabel_CellType == Set_Obs_CellType)
-}
-
-
-# 第三步：找到对应Mislabel_CellType且Method为Set_Method的Value最小的FileID
-min_value_fileID <- Summary.df %>%
-  filter(Metric == "Accuracy", Mislabel_CellType %in% Set_Obs_CellType, # Actual_Cell_Type %in% Set_Obs_CellType,
-         Method == Set_Method) %>%
-  arrange(Value) %>%
-  slice(1) %>%
-  pull(FileID)
+# # 第三步：找到对应Mislabel_CellType且Method为Set_Method的Value最小的FileID
+# min_value_fileID <- Summary.df %>%
+#   filter(Metric == "Accuracy", Mislabel_CellType %in% Set_Obs_CellType, # Actual_Cell_Type %in% Set_Obs_CellType,
+#          Method == Set_Method) %>%
+#   arrange(Value) %>%
+#   slice(1) %>%
+#   pull(FileID)
 
 ################################################################################
 library(dplyr)
@@ -143,9 +116,9 @@ Summary.df2 <- lapply(names(method_mapping), function(method) {
 # head(Summary.df2)
 
 
-# 设置 Victors_Method 和 Set_Method
-Victors_Method <- "singleR_VICTOR"  # 替换为您的实际 VICTORS 方法名称
-Set_Method <- "singleR"  # 替换为您的实际 Set 方法名称
+# # 设置 Victors_Method 和 Set_Method
+# Victors_Method <- "singleR_VICTOR"  # 替换为您的实际 VICTORS 方法名称
+# Set_Method <- "singleR"  # 替换为您的实际 Set 方法名称
 
 # 计算 Victors_Method 和 Set_Method 之间的差异，并找出最大的差异对应的 FileID 和 Actual_Cell_Type
 max_diff <- Summary.df2 %>%
@@ -159,13 +132,13 @@ max_diff <- Summary.df2 %>%
 # # 显示结果
 # max_diff
 
-min_value_fileID <- max_diff$FileID
+max_value_fileID <- max_diff$FileID
 
 ################################################################################
 
 # 第四步：使用FileID筛选TargetCell_data
 TargetCell_data <- TargetCell_data %>%
-  filter(FileID %in% min_value_fileID)
+  filter(FileID %in% max_value_fileID)
 
 if(Set_Method == "SCINA"){
   TargetCell_data <- TargetCell_data %>% filter(label_SCINA_ConfStat != "Other")
@@ -179,6 +152,8 @@ TargetCell_long <- TargetCell_data %>%
   select(Actual_Cell_Type, label_singleR_NoReject, label_singleR, label_scmap_NoReject, label_scmap, label_SCINA_NoReject, label_SCINA, label_scPred_NoReject, label_scPred) %>%
   pivot_longer(cols = -Actual_Cell_Type, names_to = "Labeling_Method", values_to = "Predicted_Cell_Type")
 
+
+
 # Modify labeling methods and order them
 TargetCell_long$Labeling_Method <- gsub("label_", "", TargetCell_long$Labeling_Method)
 TargetCell_long$Labeling_Method <- factor(TargetCell_long$Labeling_Method, levels = c("singleR_NoReject", "singleR", "scmap_NoReject", "scmap", "SCINA_NoReject", "SCINA", "scPred_NoReject", "scPred"))
@@ -188,43 +163,21 @@ TargetCell_long$Labeling_Method <- factor(TargetCell_long$Labeling_Method, level
 Platform_Query <- TargetCell_data$Sample_Platform %>% unique()
 Platform_Ref <-TargetCell_data$Ref_Platform %>% unique()
 
-# # Filter data for Set_Obs_CellType
-# TargetCell_data <- selected_data %>%
-#   filter(Mislabel_CellType == Set_Obs_CellType) %>%
-#   filter(Actual_Cell_Type == Set_Obs_CellType)
-
 
 # Prepare and reshape the data for each method
-prepare_data <- function(data, method, diag_methods, Set_CellType, Set_CellType_Reverse) {
-  if(Set_CellType_Reverse == "with"){
-    data %>%
-      filter(Actual_Cell_Type == Set_CellType) %>%
-      filter(Mislabel_CellType != Set_CellType) %>%
-      select(Predicted_Cell_Type = {{method}}, all_of(diag_methods)) %>%
-      pivot_longer(cols = -Predicted_Cell_Type, names_to = "Diag_Method", values_to = "Class") %>%
-      mutate(Method_Type = as.character(substitute(method)))
-  }else if(Set_CellType_Reverse == "Comp"){
-    data %>%
-      filter(Actual_Cell_Type == Set_CellType) %>%
-      filter(Mislabel_CellType == "None") %>%
-      select(Predicted_Cell_Type = {{method}}, all_of(diag_methods)) %>%
-      pivot_longer(cols = -Predicted_Cell_Type, names_to = "Diag_Method", values_to = "Class") %>%
-      mutate(Method_Type = as.character(substitute(method)))
-  }else{
-    data %>%
-      filter(Actual_Cell_Type == Set_CellType) %>%
-      # filter(Mislabel_CellType == Set_CellType) %>%
-      select(Predicted_Cell_Type = {{method}}, all_of(diag_methods)) %>%
-      pivot_longer(cols = -Predicted_Cell_Type, names_to = "Diag_Method", values_to = "Class") %>%
-      mutate(Method_Type = as.character(substitute(method)))
-  }
-
+prepare_data <- function(data, method, diag_methods, Set_CellType) {
+  data %>%
+    filter(Actual_Cell_Type == Set_CellType) %>%
+    # filter(Mislabel_CellType == Set_CellType) %>%
+    select(Predicted_Cell_Type = {{method}}, all_of(diag_methods)) %>%
+    pivot_longer(cols = -Predicted_Cell_Type, names_to = "Diag_Method", values_to = "Class") %>%
+    mutate(Method_Type = as.character(substitute(method)))
 }
 
-singleR_data <- prepare_data(TargetCell_data, label_singleR_NoReject, c("label_singleR_ConfStat", "ConfStat_VICTOR_label_singleR_NoReject"), Set_CellType = max_diff$Actual_Cell_Type, Set_CellType_Reverse = Set_Ref_State)
-scmap_data <- prepare_data(TargetCell_data, label_scmap_NoReject, c("label_scmap_ConfStat", "ConfStat_VICTOR_label_scmap_NoReject"), Set_CellType = max_diff$Actual_Cell_Type, Set_CellType_Reverse = Set_Ref_State)
-SCINA_data <- prepare_data(TargetCell_data, label_SCINA_NoReject, c("label_SCINA_ConfStat", "ConfStat_VICTOR_label_SCINA_NoReject"), Set_CellType = max_diff$Actual_Cell_Type, Set_CellType_Reverse = Set_Ref_State)
-scPred_data <- prepare_data(TargetCell_data, label_scPred_NoReject, c("label_scPred_ConfStat", "ConfStat_VICTOR_label_scPred_NoReject"), Set_CellType = max_diff$Actual_Cell_Type, Set_CellType_Reverse = Set_Ref_State)
+singleR_data <- prepare_data(TargetCell_data, label_singleR_NoReject, c("label_singleR_ConfStat", "ConfStat_VICTOR_label_singleR_NoReject"), Set_CellType = max_diff$Actual_Cell_Type)
+scmap_data <- prepare_data(TargetCell_data, label_scmap_NoReject, c("label_scmap_ConfStat", "ConfStat_VICTOR_label_scmap_NoReject"), Set_CellType = max_diff$Actual_Cell_Type)
+SCINA_data <- prepare_data(TargetCell_data, label_SCINA_NoReject, c("label_SCINA_ConfStat", "ConfStat_VICTOR_label_SCINA_NoReject"), Set_CellType = max_diff$Actual_Cell_Type)
+scPred_data <- prepare_data(TargetCell_data, label_scPred_NoReject, c("label_scPred_ConfStat", "ConfStat_VICTOR_label_scPred_NoReject"), Set_CellType = max_diff$Actual_Cell_Type)
 
 # Combine all method data
 combined_data <- bind_rows(singleR_data, scmap_data, SCINA_data, scPred_data) %>%
@@ -286,7 +239,7 @@ combined_data$Diag_Method <- gsub("_VICTORS", "_VICTOR", combined_data$Diag_Meth
 
 # Remove "Unassign"
 combined_data <- combined_data[!combined_data$Predicted_Cell_Type == "Unassign",]
-
+combined_data <- combined_data %>% filter(!is.na(Class))
 
 #### Bubble Plot ####
 # Load necessary libraries
@@ -331,7 +284,7 @@ ggplot(combined_data, aes(x = Predicted_Cell_Type, y = Count, fill = Class)) +
   scale_fill_manual(values = unlist(color_Class)) +
   facet_wrap(~ Diag_Method, ncol = 1) +
   theme_minimal(base_size = 14) +
-  labs(title = paste0("Annotation on ", Set_Obs_CellType,"\n", Set_Title_End),
+  labs(title = paste0("Annotation on ", max_diff$Actual_Cell_Type,"\n", Set_Title_End),
        subtitle = paste("Query:", Platform_Query," Reference:",Platform_Ref),
        x = "Predicted Cell Type", y = "Count", fill = "Class") +
   theme(panel.border = element_rect(colour = "black", fill=NA, size=1),
@@ -355,7 +308,7 @@ filtered_dataframe_FP <- combined_data %>% filter(Class == 'FP')
 
 #### Export ####
 # Name_Note <- paste0(Name_Note,"_",Set_Obs_CellType,Set_Title_End)
-Name_Note <- paste0(Set_Method,"_",min_value_fileID,"_",Set_Obs_CellType,Set_Title_End)
+Name_Note <- paste0(Set_Method,"_",max_value_fileID,"_",Set_Obs_CellType,Set_Title_End)
 
 Name_time_wo_micro <- substr(gsub("[- :]", "", as.character(Sys.time())), 1, 14)
 Name_ExportFolder <- folder_path # Name_ExportFolder <- paste0("Export_IntegAll")
